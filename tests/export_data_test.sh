ebb0c37e88a111df30de614d6029165978bd400d
peter279k <peter279k@gmail.com>
2021-12-27 15:08:35 +0800

    LICENSE, fix assertion and clickhouse-client

--- tests/export_data_test.sh
+++ tests/export_data_test.sh
@@ -5,7 +5,7 @@ test_export_data() {
     cd ..
     ./export_data.sh iotdevice01 CSV ./tests/fixtures/export_test_result.csv ./tests/fixtures/auth.txt
     assertTrue "[ -f ./tests/fixtures/export_test_result.csv ]"
-    assertSame '53.06,1000,2.6,0,11,"2021-12-23 05:06:02"' $(cat ./tests/fixtures/export_test_result.csv)
+    assertSame '53.06,1000,2.6,0,11,"2021-12-23 05:06:02"' "$(cat ./tests/fixtures/export_test_result.csv)"
 }
 
 # Load shUnit2.
